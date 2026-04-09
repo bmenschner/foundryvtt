1c30cb54c719b08a018780599c2d522419b60ff8
bmenschner <mail@bastianmenschner.de>
2026-04-09 06:16:46 +0200

    fix: restore.sh stops/starts foundry to match README flow

--- restore.sh
+++ restore.sh
@@ -5,4 +5,12 @@
 #   ./restore.sh               # neuester Stand
 #   ./restore.sh --list        # verfügbare Snapshots anzeigen
 #   ./restore.sh <commit-hash> # spezifischen Stand wiederherstellen
+
+if [ "$1" = "--list" ]; then
+    docker compose exec backup /usr/local/bin/restore.sh --list
+    exit 0
+fi
+
+docker compose stop foundry
 docker compose exec backup /usr/local/bin/restore.sh "$@"
+docker compose start foundry
